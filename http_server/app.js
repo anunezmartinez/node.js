da40d68ca93d2e8880d5f87a7bc770719bb7aa62
Adrián Núñez Martínez <55793100+anunezmartinez@users.noreply.github.com>
2020-04-12 08:23:56 +0200

    missclicked

--- http_server/app.js
+++ http_server/app.js
@@ -3,7 +3,8 @@ var dt = require('./date.js');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + dt.myDateTime());
+  res.write("The date and time are currently: " + dt.myDateTime() + "\n");
+  res.write(req.url);
   res.end();
-  
+
 }).listen(8080);
